e8df18d634cabc41693024fea13efa84dcbf7f8d
Justinefoulq <43746283+Justinefoulq@users.noreply.github.com>
2018-12-17 08:13:22 +0100

    Add files via upload

---
+++ SFChampDeBataille (1).swift
@@ -0,0 +1,99 @@
+//Champ de bataille
+
+//Champs de Bataille est une collection contenant des ZoneProtocol
+import Foundation
+protocol ChampDeBatailleProtocol: Sequence{
+    associatedtype Zone : ZoneProtocol
+    associatedtype IteratorNomZone : IteratorProtocol
+
+
+	//ligne de front, ligne arriere
+	//init:  ->ChampDeBatailleProtocol
+	//création d'un champ de bataille
+	//post-conditions: 2 lignes avec chacune 3 Zones 
+	//				   ligne de front: positions F1, F2, F3
+	//				   ligne arriere: positions A1, A2, A3
+	//Résultat: Champ de bataille avec 6 Zones vides
+	init()
+
+
+	//estVide: ChampDeBatailleProtocol -> Bool
+	//indique si un champ de bataille est vide ou non
+	//résultat: true si le champ de bataille est vide, false sinon
+	func estVide()->Bool
+
+
+	//getPositionDispo: ChampDeBatailleProtocol ->[String]
+	//renvoie les positions où il est possible de poser une carte, destiné à l'utilisateur
+	//résultat: tableau de chaines de caractères qui présente à l'utilisateur les positions où il peut poser une carte sur le champ de bataille
+	//post-conditions: positions renvoyées: F1, F2, F3 ou A1, A2, A3
+	//				   Si position sur ligne de front libre alors la position correspondante sur la ligne arrière ne l'est pas, exemple: si F1 disponible, alors même si A1 vide, elle n'est pas disponible
+	//				   Même s'il y a une carte déjà présente sur une zone, cette dernière est considérer disponible
+	func getPositionDispo()->[String]
+
+
+
+	//checkPositionDispo: String -> Bool
+	//renvoie vrai si la chaine de caractère en entrée correspond à une des positions disponible renvoyée par getPositionDispo
+	func checkPositionDispo(nomZone : String)->Bool
+
+
+	//listeAttaquant: ChampDeBatailleProtocol-> [String] | nil
+	//Résultat : renvoie un tableau de chaines de caractères comportant chaque nom de zone et une description détaillée des cartes du joueur pouvant attaquer donc étant en position défensive
+	//Renvoie nil si aucune carte ne peut attaquer
+	func listeAttaquant()-> [String]?
+
+	//initialiserPointdeDefTour: ChampDeBatailleProtocol -> ChampDeBatailleProtocol
+	//parcourt toutes les zones du champ de bataille après la phase d'action du joueur adverse et ré-initialise les points de défense de toutes les cartes du champs de bataille en fonction de leur position( defense / attaque) et de leur type d'unité
+	mutating func initialiserPointdeDefTour()
+
+    //initialiserAttaqueSoldat: ChampDeBatailleProtocol -> ChampDeBatailleProtocol
+    //parcourt toutes les zones du champ de bataille au début de la phase d'attaque du joueur adverse et ré-initialise les points d'attaque des soldats du champs de bataille en fonction du nombre de cartes qu'il ya dans la main du joueur
+    mutating func initialiserAttaqueSoldat()
+
+
+
+	//replacerCarte: ChampDeBatailleProtocol -> ChampDeBatailleProtocol
+	//Parcourt le champ de bataille et appelle la fonction mettreEnPositionDefensive sur chaque carte
+	//pre-conditions: champ de bataille non vide
+	//résultat: champ de bataille avec ses cartes en position défensive
+	mutating func replacerCarte() throws
+
+	//checkAvancement: ChampDeBatailleProtocol->
+	// Parcourt le champ de bataille et vérifie si une carte doit avancer(si une zone du front vide alors que celle derrière non), et appelle la fct avancerCarte() si c'est le cas
+	func checkAvancement()
+
+	//avancerCarte: ZoneProtocol ->
+	//avance la carte qui se trouve dans la zone passée en paramètre en ligne de front, sur la zone juste devant
+	//pré-conditions: Zone passée en paramètre est une zone arrière (A1,A2 ou A3)
+	//Résultat: carte avancée dans la zone devant elle
+	func avancerCarte(nomZone:Zone) throws
+
+	//carteAttaquable: String X ChampDeBatailleProtocol -> [Zone]
+	//Pré-conditions: Zone en entrée non vide
+	//Résultat: renvoie les zones non vides attaquables dans le champs de bataille de l'ennemie par la carte dans la zone dont le nom est entré en paramètre(utiliser getZone et getCarteZone) en fonction de la portée de la carte présente dans la zone
+    func carteAttaquable(cdb: Self ,nomZone:String) throws -> [String]
+    
+    //getZone: String -> Zone
+    //Renvoie la zone dont le nom est passé en paramètre
+    //pré-conditions: Chaîne de caactères entrée en paramètre correspond à une zone initialisée 
+    //Resultat: renvoie un zone, celle dont le nom est passé en paramètre
+    func getZone(nomZone: String) -> Zone
+	
+    //affichageCible: [String] ->
+    //Renvoie les cartes et toutes leurs propritétés détailées (point de défense restant...) des zones présentes dans la tableau de chaine de caractères passé en paramètre, qui est le résultat de carteAttaquable
+    //Résultat: une chaîne de caractères
+    func affichageCible(carteAttaquable: [String]) -> String
+
+	// makeIterator : ChampDeBatailleProtocol -> IteratorNomZone
+	// crée un itérateur sur la collection de ZoneProtocol 
+	//Résultat: Renvoie un Iterateur
+	func makeIterator() -> IteratorNomZone
+
+}
+
+
+
+
+
+
---
+++ SFjoueur (1).swift
@@ -0,0 +1,127 @@
+//JoueurProtocol
+import Foundation
+protocol JoueurProtocol{
+	//main, champ de bataille, royaume, pioche
+    associatedtype ChampDeBataille : ChampDeBatailleProtocol
+    associatedtype Carte: CarteProtocol
+    associatedtype Mains: MainsProtocol
+    associatedtype Royaume: RoyaumeProtocol
+    associatedtype Pioche: PiocheProtocol
+	//init : ->JoueurProtocol
+	//creation d'un joueur avec initialisation de: sa main, son champ de bataille, son royaume, sa pioche
+	//post-conditions pour la main: 1 roi, 3 unités piochées au hasard
+	//post-conditions pour le champ de bataille: 2 lignes de positions; ligne devant=lignede front avec positions: F1,F2,F3; ligne derriere=ligne arriere avec positions: A1,A2,A3
+	//post-conditions pourle royaume: 1 carte piochée au hasard dedans, les cartes devront être reprises dans le royaume dans l'ordre où elles sont arrivées, c'est-à-dire par exemple, 1ere carte placée dans le royaume=1ere carte à pouvoir être déployée
+	//post-conditions pour la pioche: 16 carte au hasard, au debut du jeu chaque joueur a 21 cartes dont: 1 roi, 9 soldats, 6 gardes, 5archers, dans les 16 cartes de la pioche il ne peut donc pas avoir de roi
+	init()
+
+
+	//poserCarte: JoueurProtocol x Int x String ->
+	//pose une carte correspondant à l'entier entré en paramètre (numero de carte choisi précédemment, utiliser getCarteparIdentifiant du type Mains) à la position choisie (chaine de caractère entrée en paramètre)
+	//pré-conditions: numéro de carte: entier compris entre 1 et 6 (sinon echoue)
+	//position choisie: chaîne de caractère, uniquement A1,A2, A3, F1, F2, F3, sinon échoue
+	//Si le numéro de carte entré en paramètre ne correspond à une carte présente dans la main (utiliser getMain) alors échoue (exemple: 6 entré en paramètre alors qu'il n'y a que 4 cartes dans la main)
+	// Si la position choisie ne correspond pas à une position disponible sur le champs de bataille (utiliser getChampDeBtaille) du joueur alors échoue
+	//résultat: la carte indiquée est posée à la position choisie sur le champ de bataille
+	//post-conditions: 1 carte en moins dans la main, 1 carte en plus sur le champ de bataille (à la bonne position)
+	mutating func poserCarte( identifiantCarte: Int, positionCarte: String) throws
+
+
+	//mobiliser: CarteProtocol x String -> ChampDeBatailleProtocol
+	//Prend la carte passée en paramètre dans le royaume (utiliser getRoyaume) et la place sur le champs de bataille (utiliser getChampDeBataille) à la position indiquée (utiliser getZone)
+	//Résultat: ChampDeBataille avec n cartes en plus aux positions choisies
+	//post-conditions: n cartes en moins dans le royaume, n cartes en plus sur le ChampDeBataille
+	mutating func mobiliser(CarteMobilisee: Carte, nomZone: String) throws
+
+
+
+
+	//demobiliser: Joueur x Int -> MainsProtocol 
+	//retire la carte qui a pour identifiant l'entier passé en paramètre (utiliser getCarteparIdentifiant) de la main et la place dans le royaume (utiliser getMain et getRoyaume)
+	//pré-conditions: entier compris entre 1 et 6, sinon échoue
+	//Résultat: Main avec une carte en moins
+	//Post-conditions: Main avec une carte en moins, royaume avec une carte en plus 
+	mutating func demobiliser( identifiantCarte: Int) throws -> Mains
+
+
+
+	//piocherCarte:  Joueur-> CarteProtocol
+	// pioche une carte au hasard dans la pioche (utiliser getPioche), la créer suivant la chaine de caractère renvoyé par la fonction piocher de PiocheProtocol
+	// en utilisant les fonctions de création adéquate du type CarteProtocol puis la met dans la main du joueur
+	//pré-conditions: pioche non vide
+	//résultat: 1 carte 
+	//post-conditions: nombrecarte de la pioche à baisssé de 1, 1 carte en plus dans la main
+	mutating func piocherCarte() -> Carte
+
+	 
+
+	 
+	//attaque: CarteProtocol x CarteProtocol-> Int
+	//Calcule le résultat d'une attaque entre une carteAttaquante et une carteCiblé
+	//précondition: Le premier paramètre est une carte du joueurCourant
+	//précondition: Le deuxième paramètre est une carte de l'adversaire
+	// renvoie 0 si la première carte correspondant à la carte attaquante à autant d'attaque que les points de défense actuels ( suivant qu'elles soit en position verticale ou horizontale)
+	// de la carte attaqué
+	// renvoie 1 si la première carte correspondant à la carte attaquante à plus d'attaque que les points de défense actuels ( suivant qu'elles soit en position verticale ou horizontale)
+	// de la carte attaqué
+	// renvoie 2 si la première carte correspondant à la carte attaquante à plus d'attaque que les points de défense actuels ( suivant qu'elles soit en position verticale ou horizontale)
+	// de la carte attaqué et change les pointdeDefduTour de la carte attaqué
+	mutating func attaque(carteAttaquante : Carte, carteCiblé: Carte) throws -> Int
+	
+	
+	
+	//attaquantDispo: String ->Bool 
+    //Appelle la fonction ListeAttaquant() du ChampDeBataille du joueur et Vérifie grâce au nom de la zone(chaine de caractère passée en paramètre)  si la carte qui se trouve dans cette zone (utiliser getZone de ChampDeBataille) peut attaquer
+	//Résultat: return true si la carteAttaquante(nom de ma zone où se trouve la carte ) est dans listeAttaquant() sinon renvoie false
+	func attaquantDispo(carteAttaquante: String) -> Bool
+
+
+	//checkCarteAttaque: String x [String] -> Bool
+	//Résultat: renvoie vrai si la première chaine de caractères (correspondant à la zone de la carte que veut attaquer le joueur) est dans le tableau de chaîne de caractères passé en 2e paramètre
+	// Sinon renvoie faux
+    func checkCarteAttaque(nomZoneAttaquant: String, carteAttaquableparJ: [String]) -> Bool
+	
+	
+	// estAPortee: String x String-> bool
+	//Vérifie que la carte dans la première zone (nom passé en premier paramètre) peut attaquer celle dans la zone adverse (nom passé en 2e paramètre) (utiliser getCarteZone et getZone)
+	// Pre-conditions : 1ère chaine de caractères passée en paramètre: nom de la zone dans laquelle il y a la carte attaquante, 2e chaine de caractères passée en paramètre: nom de la zone adverse ciblée
+	// Resultat : True si la cible est à portée, false sinon
+	func estAPortee(zoneAttaquant : String, zoneCible : String) -> Bool
+	
+	//checkCible: ChampDeBatailleProtocol x [String]->Bool
+	//Appelle la fonction de ChampDeBatailleProtocol listeAttaquant et regarde pour chaque carte donné par liseAttaquant si au moins une d'entre elles peut cibler une carte du champs de bataille de l'ennemie
+	//Résultat: renvoie vrai si au moins une carte de la listeAttaquant() à une cible à sa porté
+	func checkCible(champAdversaire : ChampDeBataille) -> Bool
+
+
+
+	//Capturer: JoueurProtocol x CarteProtocol ->
+	//Prend la carte passée en paramètre, la retire du champ de bataille adverse et la place dans le royaume du joueur courant
+	//Résultat: Carte en moins dans le champ de bataille adverse et 1 en plus dans le royaume
+	mutating func Capturer(carteCapturée : Carte)
+	
+    //getPioche: JoueurProtocol -> PiocheProtocol
+    //Renvoie la pioche du JoueurProtocol
+    func getPioche() -> Pioche
+    
+	
+    //getRoyaume: JoueurProtocol -> RoyaumeProtocol
+    //Renvoie le Royaume du JoueurProtocol
+    func getRoyaume() -> Royaume
+    
+    
+    
+    //getMain: JoueurProtocol -> MainsProtocol
+    //Renvoie la main du JoueurProtocol
+    func getMain() -> Mains
+    
+    
+    //getChampDeBataille: JoueurProtocol -> ChampDeBatailleProtocol
+    //Renvoie le ChampDeBataille du JoueurProtocol
+    func getChampDeBataille() -> ChampDeBataille
+    
+    
+    
+    
+
+}
